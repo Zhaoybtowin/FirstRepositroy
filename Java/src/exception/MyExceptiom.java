e3d4b8971350b31e3fca0aa7b3dfd29fac8d59af
Zhaoybtowin <zhaoyingbo_sun@163.com>
2019-12-07 23:26:06 +0800

    测试github

--- Java/src/exception/MyExceptiom.java
+++ Java/src/exception/MyExceptiom.java
@@ -5,10 +5,12 @@ package exception;
  * 1、继承于现有的异常结构：RuntimeException 、 exception
  * 2、提供全局常量：serialVersionUID（序列号）
  * 3、提供重载的构造器
+ *
  */
 public class MyExceptiom extends RuntimeException{
 
     static final long serialVersionUID = -7034897190745766939L;
+
 
     public MyExceptiom(){
 
